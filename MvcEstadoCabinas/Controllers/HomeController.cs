655cf8e8e201f2fe45f99be412292176a7783657
AndreuMartinezVidal <61981342+AndreuMartinezVidal@users.noreply.github.com>
2020-03-17 11:32:32 +0100

    Sorting by headers (asc/desc)

--- MvcEstadoCabinas/Controllers/HomeController.cs
+++ MvcEstadoCabinas/Controllers/HomeController.cs
@@ -14,9 +14,35 @@ namespace MvcEstadoCabinas.Controllers
         private PlanificacionCabinasContext db = new PlanificacionCabinasContext();
 
         // GET: PlanificacionCabinas
-        public async Task<ActionResult> Index(int days = 3, string sortOrder = "prioridad")
+        public async Task<ActionResult> Index(int days = 3, string sortOrder = "", string currentSort = "")
         {
+            ViewBag.CurrentSort = GetCurrentSort(sortOrder, currentSort);
+
             var max_date = DateTime.Today.AddDays(days);
+
+            var planificaciones = GetPlanificacionesDL(max_date);
+
+            var result = await SortEntitiesByColumnName(planificaciones).ToListAsync();
+
+            return View(result);
+        }
+
+        private string GetCurrentSort(string sortOrder, string currentSort)
+        {
+            if (!string.IsNullOrWhiteSpace(sortOrder) && currentSort == sortOrder)
+            {
+                sortOrder = sortOrder.EndsWith("_desc") ? sortOrder.Replace("_desc", "") : sortOrder + "_desc";
+            }
+            else if (sortOrder == "")
+            {
+                sortOrder = "prioridad";
+            }
+
+            return sortOrder;
+        }
+
+        private IQueryable<PlanificacionCabinasViewModel> GetPlanificacionesDL(DateTime max_date)
+        {
             var planificaciones = from p in db.Planificaciones
                                       //LEFT JOIN:
                                   join e in db.PlanificacionEstados//.Include(i=>i.PlanificacionEstadoTipo)
@@ -46,46 +72,44 @@ namespace MvcEstadoCabinas.Controllers
                                       UsuarioModificacion = estado.UsuarioModificacion,
                                       FechaModificacion = estado.FechaModificacion,
                                   };
-
-            var result = await SortEntitiesByColumnName(planificaciones,sortOrder).ToListAsync();
-
-            return View(result);
+            return planificaciones;
         }
 
-        IQueryable<PlanificacionCabinasViewModel> SortEntitiesByColumnName(IQueryable<PlanificacionCabinasViewModel> entities, string sortOrder)
+        IQueryable<PlanificacionCabinasViewModel> SortEntitiesByColumnName(IQueryable<PlanificacionCabinasViewModel> entities)
         {
+            string sortOrder = ViewBag.CurrentSort;
             if (sortOrder.EndsWith("_desc"))
                 switch (sortOrder.Replace("_desc", ""))
                 {
                     case nameof(PlanificacionCabinasViewModel.Prioridad): return entities.OrderByDescending(e => e.Prioridad);
-                    case nameof(PlanificacionCabinasViewModel.Tubo): return entities.OrderByDescending(e => e.Tubo);
-                    case nameof(PlanificacionCabinasViewModel.recurso): return entities.OrderByDescending(e => e.recurso);
-                    case nameof(PlanificacionCabinasViewModel.des_subseccion): return entities.OrderByDescending(e => e.des_subseccion);
-                    case nameof(PlanificacionCabinasViewModel.Order_cod): return entities.OrderByDescending(e => e.Order_cod);
-                    case nameof(PlanificacionCabinasViewModel.material_cod): return entities.OrderByDescending(e => e.material_cod);
-                    case nameof(PlanificacionCabinasViewModel.descart): return entities.OrderByDescending(e => e.descart);
-                    case nameof(PlanificacionCabinasViewModel.Lote): return entities.OrderByDescending(e => e.Lote);
-                    case nameof(PlanificacionCabinasViewModel.fecha_inicio_Programada): return entities.OrderByDescending(e => e.fecha_inicio_Programada);
-                    case nameof(PlanificacionCabinasViewModel.Estado): return entities.OrderByDescending(e => e.Estado);
-                    case nameof(PlanificacionCabinasViewModel.UsuarioModificacion): return entities.OrderByDescending(e => e.UsuarioModificacion);
-                    case nameof(PlanificacionCabinasViewModel.FechaModificacion): return entities.OrderByDescending(e => e.FechaModificacion);
+                    case nameof(PlanificacionCabinasViewModel.Tubo): return entities.OrderByDescending(e => e.Tubo).ThenBy(e => e.Prioridad).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.recurso): return entities.OrderByDescending(e => e.recurso).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.des_subseccion): return entities.OrderByDescending(e => e.des_subseccion).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Order_cod): return entities.OrderByDescending(e => e.Order_cod).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.material_cod): return entities.OrderByDescending(e => e.material_cod).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.descart): return entities.OrderByDescending(e => e.descart).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Lote): return entities.OrderByDescending(e => e.Lote).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.fecha_inicio_Programada): return entities.OrderByDescending(e => e.fecha_inicio_Programada).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Estado): return entities.OrderByDescending(e => e.Estado).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.UsuarioModificacion): return entities.OrderByDescending(e => e.UsuarioModificacion).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.FechaModificacion): return entities.OrderByDescending(e => e.FechaModificacion).ThenBy(e => e.Prioridad);
                     default: return entities;
                 }
             else
                 switch (sortOrder)
                 {
                     case nameof(PlanificacionCabinasViewModel.Prioridad): return entities.OrderBy(e => e.Prioridad);
-                    case nameof(PlanificacionCabinasViewModel.Tubo): return entities.OrderBy(e => e.Tubo);
-                    case nameof(PlanificacionCabinasViewModel.recurso): return entities.OrderBy(e => e.recurso);
-                    case nameof(PlanificacionCabinasViewModel.des_subseccion): return entities.OrderBy(e => e.des_subseccion);
-                    case nameof(PlanificacionCabinasViewModel.Order_cod): return entities.OrderBy(e => e.Order_cod);
-                    case nameof(PlanificacionCabinasViewModel.material_cod): return entities.OrderBy(e => e.material_cod);
-                    case nameof(PlanificacionCabinasViewModel.descart): return entities.OrderBy(e => e.descart);
-                    case nameof(PlanificacionCabinasViewModel.Lote): return entities.OrderBy(e => e.Lote);
-                    case nameof(PlanificacionCabinasViewModel.fecha_inicio_Programada): return entities.OrderBy(e => e.fecha_inicio_Programada);
-                    case nameof(PlanificacionCabinasViewModel.Estado): return entities.OrderBy(e => e.Estado);
-                    case nameof(PlanificacionCabinasViewModel.UsuarioModificacion): return entities.OrderBy(e => e.UsuarioModificacion);
-                    case nameof(PlanificacionCabinasViewModel.FechaModificacion): return entities.OrderBy(e => e.FechaModificacion);
+                    case nameof(PlanificacionCabinasViewModel.Tubo): return entities.OrderBy(e => e.Tubo).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.recurso): return entities.OrderBy(e => e.recurso).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.des_subseccion): return entities.OrderBy(e => e.des_subseccion).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Order_cod): return entities.OrderBy(e => e.Order_cod).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.material_cod): return entities.OrderBy(e => e.material_cod).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.descart): return entities.OrderBy(e => e.descart).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Lote): return entities.OrderBy(e => e.Lote).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.fecha_inicio_Programada): return entities.OrderBy(e => e.fecha_inicio_Programada).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.Estado): return entities.OrderBy(e => e.Estado).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.UsuarioModificacion): return entities.OrderBy(e => e.UsuarioModificacion).ThenBy(e => e.Prioridad);
+                    case nameof(PlanificacionCabinasViewModel.FechaModificacion): return entities.OrderBy(e => e.FechaModificacion).ThenBy(e => e.Prioridad);
                     default: return entities;
                 };
         }
